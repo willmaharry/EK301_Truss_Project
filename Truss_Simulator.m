f650c866cdbc5d13a3118903cf2ade99333959b9
willmaharry <wmaharry@bu.edu>
2023-11-07 12:13:05 -0500

    Connection Matrix
    Connection Matrix!! And error checking

--- Truss_Simulator.m
+++ Truss_Simulator.m
@@ -1,2 +1,19 @@
 %Preliminary Design Review Code:
-C = [0,1];
+
+%Definition of truss parameters
+
+%Connection matrix, rows are the members and columns are the joints
+C = [1 1 0 0 0 0 0;
+    1 0 1 0 1 1 0;
+    0 1 1 1 0 0 0;
+    0 0 0 1 1 0 1;
+    0 0 0 0 0 1 1];
+
+%Check the connection matrix is properly defined (M = 2J-3)
+if width(C) ~= 2*height(C) - 3
+    fprintf("Incorrect dimensions\n")
+end
+if ~all(sum(C) == 2)
+    fprintf("Not all columns add to 2\n")
+end
+
