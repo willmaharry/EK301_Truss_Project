614a3a8deb55b3dcc7c4dd9e638e2bc17e8f0d18
willmaharry <wmaharry@bu.edu>
2023-11-15 18:30:23 -0500

    Fixed Signs for tension and compression

--- Truss_Simulator.m
+++ Truss_Simulator.m
@@ -3,117 +3,9 @@
 clc;clear;
 load("PracticeProblemInput.mat");
 
-%Definition of truss parameters
-%{
-joints = 8;  %Number of rows of C
-members = 13;  %Number of columns of C
-
-%Connection matrix, members are the columns and joints are the rows (M<J)
-
-C = [
-     1 1 0 0 0 0 0 0 0 0 0 0 0;
-     1 0 1 1 0 0 0 0 0 0 0 0 0;
-     0 0 0 1 1 0 0 1 0 0 0 0 0;
-     0 1 1 0 1 1 1 0 0 0 0 0 0;
-     0 0 0 0 0 0 1 1 1 0 0 1 0;
-     0 0 0 0 0 1 0 0 1 1 1 0 0;
-     0 0 0 0 0 0 0 0 0 0 1 1 1;
-     0 0 0 0 0 0 0 0 0 1 0 0 1;
-    ];
-
-%Check the connection matrix is properly defined (M = 2J-3)
-if width(C) ~= 2*height(C) - 3
-    fprintf("Incorrect dimensions\n")
-end
-if ~all(sum(C) == 2)
-    fprintf("Not all columns add to 2\n")
-end
-
-%Support force matrices, each should have J rows and 3 columns
-%Which joint has the x reaction force?
-sx =   [
-        1 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        ];
-
-if height(sx) ~= height(C)
-    fprintf("sx has incorrect dimensions\n")
-end
-if ~isequal(sum(sx),[1,0,0])
-    fprintf("Invalid inputs for sx\n")
-end
-
-%Which joints have the y reaction forces?
-sy =   [
-        0 1 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 0;
-        0 0 1;
-        ];
-
-if height(sy) ~= height(C)
-    fprintf("sy has incorrect dimensions\n")
-end
-if ~isequal(sum(sy),[0,1,1])
-    fprintf("Invalid inputs for sy\n")
-end
-
-%Location vectors X and Y. (0,0) is defined as the joint where both X and Y
-%reaction forces act
-%x and y are measured in inches
-x = [0 0 4 4 8 8 12 12];
-if width(x) ~= height(C)
-   fprintf("x has incorrect dimensions\n")
-end
-
-y = [0 4 8 4 8 4 4 0];
-if width(y) ~= height(C)
-   fprintf("y has incorrect dimensions\n")
-end
-
-%Load vector L, measured in Oz.
-
-L = [   %X external loads  (signs should be positive?)
-        0;   
-        0;  
-        0;   
-        0;   
-        0;
-        0;
-        0;
-        0;
-        % Y external loads (signs should be positive?)
-        0;   
-        0;  
-        0;   
-        25; % y down of 1 unit
-        0;
-        0;
-        0;
-        0;
-        ];
-
-if height(L) ~= 2*height(C)
-    fprintf("L has incorrect dimensions\n")
-end
-if ~any(L)
-    fprintf("Invalid inputs for L\n")
-end
-
-%}
-
 %First, lets just analyze the truss and display the results
 T = trussCalculator(C, Sx, Sy, X, Y, L);
+T = round(T,3);
 
 %Calculate the cost of the truss: I'll just make another function
 cost = calculateCost(C,X,Y);
@@ -137,10 +29,10 @@ for i = 1:height(T)
     if i < height(T)-2
         % I flipped tension and compresssion but it's fine cuz I can just
         % rewrite it here
-        if T(i) < 0
-            disp("Member " + string(i) + ": " + string(-1*T(i)) + " oz. (C)")
+        if T(i) > 0
+            disp("Member " + string(i) + ": " + string(T(i)) + " oz. (C)")
         else
-            disp("Member " + string(i) + ": " + string(T(i)) + " oz. (T)")
+            disp("Member " + string(i) + ": " + string(-1*T(i)) + " oz. (T)")
         end
     else 
         if i == height(T)-2
@@ -170,9 +62,9 @@ maxLoad = 100; % This signifies "units" in compresssion, we ignore tension
 
 maxTests = [0, 0, 0];
 maxReached = 0;
-jointLoad = -1;
-newJointLoad = -1;
-prevT = [];
+jointLoad = 1;
+newJointLoad = 1;
+prevTmax = [];
 while ~maxReached
     if maxTests(1) == 0
         newJointLoad = jointLoad * 2;
@@ -181,8 +73,8 @@ while ~maxReached
     elseif maxTests(3) == 0
         newJointLoad = jointLoad * 1.01;
     end
-    T = trussCalculator(C, Sx, Sy, X, Y, loadModifier(L, newJointLoad));
-    if surpassedMaxLoad(T, maxLoad)
+    Tmax = trussCalculator(C, Sx, Sy, X, Y, loadModifier(L, newJointLoad));
+    if surpassedMaxLoad(Tmax, maxLoad)
         if maxTests(1) == 0
             maxTests(1) = 1;
         elseif maxTests(2) == 0
@@ -190,11 +82,11 @@ while ~maxReached
         elseif maxTests(3) == 0
             maxTests(3) = 1;
             maxReached = 1;
-            T = prevT;
+            Tmax = prevTmax;
         end
     else
         jointLoad = newJointLoad;
-        prevT = T;
+        prevTmax = Tmax;
     end
 end
 
@@ -204,4 +96,3 @@ maxLoadToCostRatio = abs(jointLoad)/cost;
 % Now print out the results: 
 disp("The Max Load at joint " + string(loadJoint) + " : " + string(jointLoad));
 disp("TheoRHETTical max load/cost ratio in oz/$: " + maxLoadToCostRatio)
-
