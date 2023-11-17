367f69fcc3e62b215433518ed6a208b329ec1a9f
willmaharry <wmaharry@bu.edu>
2023-11-17 13:02:03 -0500

    Update to memberMaxLoadFinder, nothing major

--- memberMaxLoadFinder.m
+++ memberMaxLoadFinder.m
@@ -1,11 +1,10 @@
 function memberMaxLoads = memberMaxLoadFinder(C, X, Y)
     % We also want to calculate the length of each member to use it in the
     % strength calculations
-    Csize = size(C);
-    memberLengthsMat = zeros(Csize(1), Csize(2));
+    memberLengths = zeros(1,width(C));
     % first make a matrix version based on C
-    for i = 1:Csize(1)
-        for j = 1:Csize(2)
+    for i = 1:height(C)
+        for j = 1:width(C)
             if C(i,j) == 1
                 % I gotta find the other connected memeber, gonna do a function
                 % call for this one
@@ -22,38 +21,14 @@ function memberMaxLoads = memberMaxLoadFinder(C, X, Y)
                 m2pos = [X(otherJoint), Y(otherJoint)];
     
                 % And compute the radial distance
-                memberLengthsMat(i, j) = sqrt((m2pos(1)-m1pos(1))^2 + (m2pos(2)-m1pos(2))^2);
-            end
-        end
-    end
-    % now flatten that mofo
-    memberLengths = zeros(1, Csize(2));
-    for i = 1:Csize(2)
-        tmp = 0;
-        for j = 1:Csize(1)
-            if (tmp == 0) && (memberLengthsMat(j, i) ~= 0)
-                tmp = memberLengthsMat(j, i);
-            elseif (tmp ~= 0) && (memberLengthsMat(j, i) ~= 0)
-                if (tmp ~= memberLengthsMat(j, i))
-                   disp("ERROR: Member lengths don't match.") 
-                   break;
-                else
-                    memberLengths(i) = tmp;
-                end
+                memberLengths(j) = sqrt((m2pos(1)-m1pos(1))^2 + (m2pos(2)-m1pos(2))^2);
             end
         end
     end
     % and find the max loads
-    memberMaxLoads = zeros(1, Csize(2));
-    for i = 1:Csize(2)
-        memberMaxLoads(i) = 3654.553 * memberLengths(i)^-2.119;
+    memberMaxLoads = zeros(1, width(C));
+    for i = 1:width(C)
+        memberMaxLoads(i) = 3654.553 * (memberLengths(i)^-2.119);
     end
 end
-
-
-
-
-
-
-
 
--- surpassedMaxLoad.m
+++ surpassedMaxLoad.m
@@ -1,8 +1,8 @@
-function surpassed = surpassedMaxLoad(L, memberMaxLoads)
+function surpassed = surpassedMaxLoad(Tmax, memberMaxLoads)
     surpassed = 0;
-    for i = 1:height(L)-3 % So we don't accidentally calculate the S forces
+    for i = 1:height(Tmax)-3 % So we don't accidentally calculate the S forces
         % Calculate Max load for member i
-        if (L(i) > memberMaxLoads(i))
+        if (Tmax(i) > memberMaxLoads(i))
             surpassed = 1;
         end
     end    
